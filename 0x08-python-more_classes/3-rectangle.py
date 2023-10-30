4d4a85ad84031aa1344118e0b8120e1dee64c1a8
ElmourikiYassine <yassineelmouriki@gmail.com>
2023-10-30 11:31:09 -0700

    0x08_task_3

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -52,4 +52,5 @@ class Rectangle:
         return "\n".join(['#' * self.__width for _ in range(self.__height)])
 
     def __repr__(self):
-        return "Rectangle({}, {})".format(self.__width, self.__height)
+        return "<3-rectangle.{} object at {}>".format(
+                self.__class__.__name__, hex(id(self)))
