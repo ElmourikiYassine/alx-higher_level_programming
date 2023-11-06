ceb527404f9302cc2298102c5984c608683f2e70
ElmourikiYassine <yassineelmouriki@gmail.com>
2023-11-06 04:51:57 -0800

    0x0A_task_5

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module with a lookup class"""
+
+
+class BaseGeometry():
+    """
+        Inherits from list and provides a
+        method to print sorted list.
+    """
+    pass
