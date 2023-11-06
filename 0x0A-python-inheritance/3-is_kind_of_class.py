0a29b904a1fa6a3335771364832cbe92aac65ed3
ElmourikiYassine <yassineelmouriki@gmail.com>
2023-11-06 04:38:22 -0800

    0x0A_task_3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module with a lookup class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+        Inherits from list and provides a
+        method to print sorted list.
+    """
+    return isinstance(obj, a_class)
