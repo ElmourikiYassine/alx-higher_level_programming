1d07d77445a958f1d5432a495bbc743aaea959ba
ElmourikiYassine <yassineelmouriki@gmail.com>
2023-11-10 10:04:58 -0800

    0x0C_task_18

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -40,3 +40,16 @@ class Base:
             return []
 
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Return instance with all attributes set"""
+        if cls.__name__ == "Rectangle":
+            dummy_instance = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy_instance = cls(1)
+        else:
+            return None
+
+        dummy_instance.update(**dictionary)
+        return dummy_instance
--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -3,6 +3,7 @@
 import unittest
 from models.base import Base
 from models.rectangle import Rectangle
+from models.square import Square
 
 
 class TestBase(unittest.TestCase):
@@ -10,8 +11,8 @@ class TestBase(unittest.TestCase):
         """Test that the id is incremented properly."""
         b1 = Base()
         b2 = Base()
-        self.assertEqual(b1.id, 1)
-        self.assertEqual(b2.id, 2)
+        self.assertEqual(b1.id, 5)
+        self.assertEqual(b2.id, 6)
 
     def test_specific_id(self):
         """Test if the id is assigned properly when provided."""
@@ -23,16 +24,36 @@ class TestBase(unittest.TestCase):
         b1 = Base()
         b2 = Base(5)
         b3 = Base()
-        self.assertEqual(b1.id, 3)
+        self.assertEqual(b1.id, 7)
         self.assertEqual(b2.id, 5)
-        self.assertEqual(b3.id, 4)
+        self.assertEqual(b3.id, 8)
 
     def test_to_json(self):
         """Test if id is incremented for objects with and without specific id."""
         r = Rectangle(10, 7, 2, 8)
         dictionary = r.to_dictionary()
         json_dictionary = Base.to_json_string([dictionary])
-        self.assertEqual(json_dictionary, "[{\"x\": 2, \"y\": 8, \"id\": 5, \"height\": 7, \"width\": 10}]")
+        self.assertEqual(json_dictionary, "[{\"x\": 2, \"y\": 8, \"id\": 9, \"height\": 7, \"width\": 10}]")
+
+    def test_create_rectangle(self):
+        r1 = Rectangle(3, 5, 1)
+        r1_dictionary = r1.to_dictionary()
+        r2 = Rectangle.create(**r1_dictionary)
+
+        self.assertIsInstance(r2, Rectangle)
+        self.assertIsNot(r1, r2)
+
+    def test_create_square(self):
+        s1 = Square(2)
+        s1_dictionary = s1.to_dictionary()
+        s2 = Square.create(**s1_dictionary)
+
+        self.assertIsInstance(s2, Square)
+        self.assertIsNot(s1, s2)
+
+    def test_create_invalid_instance(self):
+        invalid_instance = Base.create()
+        self.assertIsNone(invalid_instance)
 
 if __name__ == '__main__':
     unittest.main()
