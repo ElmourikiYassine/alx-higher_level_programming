97515b34ffe331b0a3ba135fc44294858918f2e9
ElmourikiYassine <yassineelmouriki@gmail.com>
2023-12-14 05:41:43 -0800

    task 2

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,8 @@
+-- Create database hbtn_0d_2 if not exists
+-- Create user_0d_2 if not exists
+-- Grant SELECT privilege on hbtn_0d_2 to user_0d_2
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT USAGE ON *.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
+
