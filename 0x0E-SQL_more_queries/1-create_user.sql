c8413c93b42338cbae3576ef20d26e47cab690c6
ElmourikiYassine <yassineelmouriki@gmail.com>
2023-12-14 05:35:41 -0800

    fixing task 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,5 @@
--- Create user_0d_1 if not exists and Grant all privileges to user_0d_1
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd'
+-- Create user_0d_1 if not exists
+-- Grant all privileges to user_0d_1
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
