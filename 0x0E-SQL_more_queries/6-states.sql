25d6a4d929b00ffb141d0c39b8997f384d148622
ElmourikiYassine <yassineelmouriki@gmail.com>
2023-12-14 06:18:53 -0800

    task 6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- Create database hbtn_0d_usa if not exists and create table states if not exists
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+	id INT UNIQUE AUTO_INCREMENT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	PRIMARY KEY (id)
+);
