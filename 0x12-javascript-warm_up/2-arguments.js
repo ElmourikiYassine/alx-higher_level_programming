b15cd73b3381098054c8e0867150e0ee0047666e
elmouirkiyassine <yassineelmouriki@gmail.com>
2024-01-09 02:12:20 -0800

    fixing task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,8 +2,10 @@
 
 const { argv } = require('node:process');
 
-if (argv[2]) {
+if (argv.length === 3) {
   console.log('Argument found');
+} else if (argv.length > 3) {
+  console.log('Arguments found');
 } else {
   console.log('No argument');
 }
