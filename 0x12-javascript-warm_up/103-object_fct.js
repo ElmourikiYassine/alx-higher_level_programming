d935cd935fddd390218c607f88a90bbf4ca5fd29
elmouirkiyassine <yassineelmouriki@gmail.com>
2024-01-09 06:20:54 -0800

    fixing task 103

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,14 +1,13 @@
 #!/usr/bin/node
+
 const myObject = {
   type: 'object',
   value: 12
 };
 console.log(myObject);
-
-myObject.incr = function incr () {
-  this.value += 1;
+myObject.incr = function () {
+  this.value++;
 };
-
 myObject.incr();
 console.log(myObject);
 myObject.incr();
