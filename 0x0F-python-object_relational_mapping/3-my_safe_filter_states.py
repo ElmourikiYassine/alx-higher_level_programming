dc23d83dbeee444c75c72594ddd8e762740f343f
elmourikiyassine <yassinelemouriki@gmail.com>
2024-01-12 08:23:29 -0800

    task_3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Script to list states safely based on user
+   input from the database hbtn_0e_0_usa"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    (username, password, database, user_input) = (
+            sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+
+    db = MySQLdb.connect(
+            host='localhost', port=3306,
+            user=username, passwd=password, db=database)
+
+    cur = db.cursor()
+
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+
+    cur.execute(query, (user_input,))
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
