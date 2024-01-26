23155ac92516eb60c2911fe8addbc7d0bd45ef24
ElmourikiYassine <yassineelmouriki@gmail.com>
2024-01-26 05:13:06 -0800

    fixing_5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a POST request to the passed URL, and displays the body of the response
- curl -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" $1
+# This script sends a POST request to the URL passed as the first argument
+curl -sX POST "$1" -d "email=test@gmail.com&subject=I will always be here for PLD"
