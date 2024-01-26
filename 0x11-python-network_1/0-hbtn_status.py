f5b44b21b2d8428292cd473cfec2125f7e5518c1
ElmourikiYassine <yassineelmouriki@gmail.com>
2024-01-26 10:00:54 -0800

    task_0_fixing

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """a Python script that fetches https://alx-intranet.hbtn.io/status"""
-
-
 import urllib.request
 
-if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status')\
-            as response:
-        print(f"Body respoonse:\n\t\
-- type: {type(response)}\n\t\
-- content: {response.read()}\n\t\
-- utf8 content: {'Ok' if response.readable() else 'No'}")
+url = "https://alx-intranet.hbtn.io/status"
+
+with urllib.request.urlopen(url) as response:
+    content = response.read()
+
+print("Body response:")
+print(f"\t- type: {type(content)}")
+print(f"\t- content: {content}")
+print(f"\t- utf8 content: {content.decode('utf-8')}")
