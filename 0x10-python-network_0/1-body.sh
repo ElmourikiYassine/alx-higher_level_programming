97144e71e61e1caa3c61398e0a5ec4b11b2aa1a9
ElmourikiYassine <yassineelmouriki@gmail.com>
2024-01-26 04:17:56 -0800

    fixing task 2 & 3

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # This script sends a GET request to the URL passed as the first argument
-# and displays the body of the response only if it is a 200 status code response.
-
 curl -sL "$1"
