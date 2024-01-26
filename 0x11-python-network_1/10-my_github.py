5e236fea308d4a4611424f512288af72e8cd2f89
ElmourikiYassine <yassineelmouriki@gmail.com>
2024-01-26 14:54:09 -0800

    Task_10

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Authentication with a personal access token as
+    password to access GitHub user information.
+    It then prints the user ID extracted from the JSON response."""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    token = sys.argv[2]
+    url = "https://api.github.com/user"
+
+    response = requests.get(url, auth=(username, token))
+    if response.status_code == 200:
+        print(response.json().get('id'))
+    else:
+        print(None)
